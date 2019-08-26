6743a910816309257286f0ea7b37db099fbce6fa
afedchenko <47084978+afedchenko@users.noreply.github.com>
2019-08-26 17:46:12 +0700

    Update Course.java

--- 1stLesson/src/Course.java
+++ 1stLesson/src/Course.java
@@ -1,7 +1,25 @@
 public class Course {
-    /**
+     /**
      * 3. Добавить класс Course (полоса препятствий), в котором будут находиться массив препятствий и метод,
      * который будет просить команду пройти всю полосу;
      */
 
+    private int[] board;
+
+    public Course(int[] board) {
+        this.board = board;
+    }
+
+
+    public void doIt(Team team) {
+        for (TeamMate i : team.getTeamMate()) {
+
+            for (int j = 0; j <= board.length; j++) {
+                if(i.getJump()<j){
+                    i.result = 0;
+                }
+            }
+
+        }
+    }
 }
