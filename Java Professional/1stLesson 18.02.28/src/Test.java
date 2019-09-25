0808da80ec3451ee3f6e19647473128ab162cfd9
Alexandr Fedchenko <a.fedchenko@n1.ru>
2019-09-25 18:04:02 +0700

    Сделал перестановку элементов массива

---
+++ Java Professional/1stLesson 18.02.28/src/Test.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Test {
+    //1. Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
+    private static <T> void swap(T[] a) {
+        T t = a[0];
+        a[0] = a[1];
+        a[1] = t;
+    }
+
+    private static <T> void swap(List<T> l) {
+        Collections.<T>swap(l, 0, 1);
+    }
+
+    private static void test() {
+        String[] a = {"Hello", "Goodbye"};
+        swap(a);
+        System.out.println("a:" + Arrays.toString(a));
+        List<String> l = new ArrayList<String>(Arrays.asList(a));
+        swap(l);
+        System.out.println("l:" + l);
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
+//2. Написать метод, который преобразует массив в ArrayList;
