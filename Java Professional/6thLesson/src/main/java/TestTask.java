f1538c16519d24c1b0cea45985a3876035cb0b71
Alexandr Fedchenko <a.fedchenko@n1.ru>
2019-10-21 12:58:40 +0700

    Доделал тесты для первой задачи

--- Java Professional/6thLesson/src/main/java/TestTask.java
+++ Java Professional/6thLesson/src/main/java/TestTask.java
@@ -1,8 +1,10 @@
+package main.java;
+
 import java.util.Arrays;
 
-public class Test {
-    public static void main(String[] args) {
-        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 4, 4, 4, 6, 8, 9};
+public class TestTask {
+    public void main(String[] args) {
+        int[] array = new int[]{4, 1, 44, 3, 6, 8, 6, 8, 9};
         int[] array1 = new int[]{1, 2, 3, 5, 6, 7, 6, 0, 9, 5};
 
         try {
@@ -13,7 +15,7 @@ public class Test {
         }
     }
 
-    public static int[] arrayAfterLastFour(int[] a) throws RuntimeException {
+    public int[] arrayAfterLastFour(int[] a) throws RuntimeException {
         int index = 0;
         boolean thereIsFour = false;
 
@@ -28,14 +30,13 @@ public class Test {
         //Вычисляем длину нового массива = длина старного массива - длинна массива до последней четверки включительно
         int[] checkedArray = new int[a.length - index];
 
-        if (thereIsFour == false) {
+        if (!thereIsFour) {
             throw new RuntimeException();
         } else {
             //Если есть четверки в массиве, то заполняем новый массив числами из старого массива после последней четверки
             for (int i = 0; i < (a.length - index); i++) {
                 checkedArray[i] = a[index + i];
             }
-            System.out.println("Цифры после четверки");
         }
         return checkedArray;
     }
