680572f4df5c4bc5163c31e958939eba4b8dfb56
Alexandr Fedchenko <a.fedchenko@n1.ru>
2019-11-14 17:52:32 +0700

    Написал интенты для свитчей с экрана настроек. Применяю изменение свитчей в mainActivity. Передаю изменение данных из mainActivity назад в настройки.

--- Android Tasks/AndroidTask/app/src/main/java/com/example/weather/MainActivity.java
+++ Android Tasks/AndroidTask/app/src/main/java/com/example/weather/MainActivity.java
@@ -1,34 +1,97 @@
 package com.example.weather;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private final int weatherSettingsActivityResultCode = 7;
+    private boolean humidityEnabled = true;
+    private boolean pressureEnabled = true;
+    private boolean windSpeedEnabled = true;
+    private LinearLayout humidity, pressure, windSpeed;
+    private Button browser, settings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         showLog("OnCreate");
         setContentView(R.layout.activity_main);
-        findViewById(R.id.activity_main_button_setings).setOnClickListener(new View.OnClickListener() {
+
+        //Инициализируем view кнопку настроек и открываем по ней экран настроек
+        settings = findViewById(R.id.activity_main_button_setings);
+        settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 clickOnSettingsButton();
             }
         });
+
+        //Инициализируем view кнопку браузера и открываем по ней браузер
+        browser = findViewById(R.id.activity_main_button_browser);
+        browser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://pogoda.ngs.ru/"));
+                startActivity(openBrowser);
+            }
+        });
+
+        //Инициализируем view параметров погоды
+        humidity = findViewById(R.id.activity_main_linear_layout_humidity);
+
+        pressure = findViewById(R.id.activity_main_linear_layuot_pressure);
+
+        windSpeed = findViewById(R.id.activity_main_linear_layout_wind_speed);
+
     }
 
-    void clickOnSettingsButton() {
-        startActivityForResult(new Intent(this, WeatherSettingsActivity.class), 7);
+    private void clickOnSettingsButton() {
+        Intent intent = new Intent(this, WeatherSettingsActivity.class);
+        intent.putExtra(WeatherSettingsActivity.PRESSURE, pressureEnabled);
+        intent.putExtra(WeatherSettingsActivity.HUMIDITY, humidityEnabled);
+        intent.putExtra(WeatherSettingsActivity.WIND_SPEED, windSpeedEnabled);
+
+        startActivityForResult(intent, weatherSettingsActivityResultCode);
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK && requestCode == weatherSettingsActivityResultCode) {
+            updateWeatheParams(data);
+        }
+    }
+
+    private void updateWeatheParams(@Nullable Intent data) {
+        humidityEnabled = data.getBooleanExtra(WeatherSettingsActivity.HUMIDITY, false);
+        pressureEnabled = data.getBooleanExtra(WeatherSettingsActivity.PRESSURE, false);
+        windSpeedEnabled = data.getBooleanExtra(WeatherSettingsActivity.WIND_SPEED, false);
+
+        checkValues();
+    }
+
+    private void checkValues() {
+        changeVisibilityView(humidityEnabled, humidity);
+        changeVisibilityView(pressureEnabled, pressure);
+        changeVisibilityView(windSpeedEnabled, windSpeed);
+    }
+
+    //Проверяем, что нам пришло из настроек, и в зависимости от true/false скрываем или показываем view
+    private void changeVisibilityView (boolean isChecked, LinearLayout layout) {
+        if (isChecked) {
+            layout.setVisibility(View.VISIBLE);
+        } else layout.setVisibility(View.GONE);
+    }
 
 
     @Override
--- Android Tasks/AndroidTask/app/src/main/java/com/example/weather/WeatherSettingsActivity.java
+++ Android Tasks/AndroidTask/app/src/main/java/com/example/weather/WeatherSettingsActivity.java
@@ -1,12 +1,13 @@
 package com.example.weather;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +15,13 @@ import androidx.annotation.Nullable;
 public class WeatherSettingsActivity extends Activity {
     private static final String TAG = "WeatherSettingsActivity";
     private EditText cityName;
-
-
     private static String CITY_NAME = "city_name";
+    private Switch humidity, pressure, windSpeed;
+    private Button backButton;
+
+    public static final String HUMIDITY = "WeatherSettingsActivityHumidity";
+    public static final String PRESSURE = "WeatherSettingsActivityPressure";
+    public static final String WIND_SPEED = "WeatherSettingsActivityWindSpeed";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,9 +29,17 @@ public class WeatherSettingsActivity extends Activity {
         showLog("OnCreate");
         setContentView(R.layout.weather_settings);
 
-        findViewById(R.id.weather_settings_button_back).setOnClickListener(new View.OnClickListener() {
+        humidity = findViewById(R.id.weather_settings_humidity);
+
+        pressure = findViewById(R.id.weather_settings_pressure);
+
+        windSpeed = findViewById(R.id.weather_settings_wind_speed);
+
+        backButton = findViewById(R.id.weather_settings_button_back);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 clickOnBackButton();
             }
         });
@@ -34,15 +47,36 @@ public class WeatherSettingsActivity extends Activity {
         cityName = findViewById(R.id.weather_settings_city_other_change);
         restoreData(savedInstanceState);
 
-
+        getSwitchStateFromMain();
     }
 
-    void clickOnBackButton(){
+    @Override
+    public void onBackPressed() {
+        clickOnBackButton();
+    }
+
+    private void getSwitchStateFromMain() {
+        humidity.setChecked(getIntent().getBooleanExtra(HUMIDITY, false));
+        pressure.setChecked(getIntent().getBooleanExtra(PRESSURE, false));
+        windSpeed.setChecked(getIntent().getBooleanExtra(WIND_SPEED, false));
+    }
+
+    private void prepareResult() {
+        Intent intent = new Intent();
+        intent.putExtra(PRESSURE, pressure.isChecked());
+        intent.putExtra(HUMIDITY, humidity.isChecked());
+        intent.putExtra(WIND_SPEED, windSpeed.isChecked());
+
+        setResult(RESULT_OK, intent);
+    }
+
+    private void clickOnBackButton() {
+        prepareResult();
         finish();
     }
 
     private void restoreData(Bundle savedInstanceState) {
-        if(savedInstanceState == null) return;
+        if (savedInstanceState == null) return;
         cityName.setText(savedInstanceState.getString(CITY_NAME, getString(R.string.novosibirsk)));
     }
 
@@ -51,6 +85,8 @@ public class WeatherSettingsActivity extends Activity {
         super.onSaveInstanceState(outState);
         outState.putString(CITY_NAME, cityName.getText().toString());
     }
+
+    //Выводим логи по событиям активностей
 
     @Override
     protected void onStart() {
